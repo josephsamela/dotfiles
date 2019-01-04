61b111fe0e0be70527aae5739cafdf844578e1f2
Joseph Samela <josephsamela@gmail.com>
2019-01-03 22:45:30 -0500

    Update setup_debian.sh

--- setup_debian.sh
+++ setup_debian.sh
@@ -1,6 +1,8 @@
 # Joe Samela's setup script for debian, ubuntu, elementary, raspbian, etc. 
 
+# Install most commonly used programs
 apt install tmux neovim ranger moc sxic screenfetch tree git ffmpeg mutt pass
 
-echo "alias p='cd /media/removable/STORAGE/Projects'" >> /home/user/.bashrc
-echo "alias vim='nvim'" >> /home/user/.bashrc
+# Set important alias
+echo "alias p='cd /media/removable/STORAGE/Projects'" >> ~/.bashrc
+echo "alias vim='nvim'" >> ~/.bashrc

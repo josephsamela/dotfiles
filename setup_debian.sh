f3387bc74cc6ecf17ebeb1ff96d68215c6a26fbc
Joseph Samela <josephsamela@gmail.com>
2019-01-09 10:54:01 -0500

    Update setup_debian.sh

--- setup_debian.sh
+++ setup_debian.sh
@@ -5,9 +5,14 @@ apt update
 apt upgrade
 
 # Install most commonly used programs
-apt install tmux htop neovim ranger tree sxic moc screenfetch git ffmpeg mutt pass
+apt install tmux htop neovim ranger tree sxic moc screenfetch git ffmpeg mutt pass python3
 
 # Set important alias
 echo "alias p='cd /media/removable/STORAGE/Projects'" >> ~/.bashrc
 echo "alias vim='nvim'" >> ~/.bashrc
 echo "alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'" >> ~/.bashrc
+echo "alias python='python3'" >> ~/.bashrc
+echo "alias pip='python3 -m pip'" >> ~/.bashrc
+
+# Install python packages
+pip install castero

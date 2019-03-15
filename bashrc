47705004c5c2f4019394470bf1c630c96678ef64
Joseph Samela <josephsamela@gmail.com>
2019-03-14 20:43:49 -0400

    move config to seperate files

---
+++ bashrc
@@ -0,0 +1,11 @@
+alias p='cd ~/projects'
+alias vim='nvim'
+alias ranger='ranger --choosedir=/home/josephsamela/.rangerdir; LASTDIR=; cd '
+alias python='python3'
+alias pip='python3 -m pip'
+alias weather='curl wttr.in'
+alias vrc='vim ~/.config/nvim/init.vim'
+alias brc='vim ~/.bashrc'
+alias trc='vim ~/.tmux.conf'
+alias mocp="mocp --theme nes"
+alias ll="ls -l"
--- setup_debian.sh
+++ setup_debian.sh
@@ -3,7 +3,7 @@
 # Reminder to run this as root!
 if [ ! "`whoami`" = "root" ]
 then
-    echo "\nPlease run script as root."
+    echo "Please run script as root."
     exit 1
 fi
 
@@ -12,25 +12,16 @@ apt update
 apt upgrade
 
 # Install most commonly used programs
-apt install tmux htop neovim ranger tree moc neofetch git ffmpeg python3 python3-pip
+apt install tmux htop neovim ranger tree moc neofetch git ffmpeg python3 python3-pip unzip
 
-# Set important alias in ~/.bashrc
-echo "alias p='cd ~/projects'" >> ~/.bashrc
-echo "alias vim='nvim'" >> ~/.bashrc
-echo "alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'" >> ~/.bashrc
-echo "alias python='python3'" >> ~/.bashrc
-echo "alias pip='python3 -m pip'" >> ~/.bashrc
-echo "alias p='cd ~/projects'" >> ~/.bashrc
-echo "alias weather='curl wttr.in'" >> ~/.bashrc
-echo "alias vrc='vim ~/.config/nvim/init.vim'" >> ~/.bashrc
-echo "alias brc='vim ~/.bashrc'" >> ~/.bashrc
-echo "alias trc='vim ~/.tmux.conf'" >> ~/.bashrc
-echo "alias ll='ls -l'" >> ~/.bashrc
-echo "alias la='ls -a'" >> ~/.bashrc
+# Set bashrc config
+cp bashrc ~/.bashrc
+
+# Set mocp config
+cp -R moc ~/.moc
 
 # Set tmux preferences in ~/.tmux.conf
-echo "prefix : set -g mouse off" >> ~/.tmux.conf
-echo "set -sg escape-time 0" >> ~/.tmux.conf
+cp tmux.conf ~/.tmux.conf
 
 # neovim preferences to ~/.config
 cp -R nvim/ ~/.config
@@ -41,6 +32,3 @@ pip install castero
 # Create project folder
 mkdir ~/projects
 
-# Install note
-git clone https://github.com/JosephSamela/note ~/projects/note
-bash ~/projects/note/install.sh

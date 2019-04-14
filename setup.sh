1aeb9f65239e4246313711ea84c51eb4be2ca1dd
Joseph Samela <josephsamela@gmail.com>
2019-04-14 16:35:03 -0400

    prevent permission errors

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 # Joe Samela's setup script for debian, ubuntu, elementary, raspbian, etc. 
 
-# Reminder to run this as root!
-if [ ! "`whoami`" = "root" ]
+# Reminder to NOT run as root!
+if [ "`whoami`" = "root" ]
 then
-    echo "Please run script as root."
+    echo "Please do not run this script as root."
     exit 1
 fi
 
 # Set bashrc config
-echo bashrc >> ~/.bashrc
+cat bashrc >> ~/.bashrc
 
 # Set mocp config
 cp -R moc ~/.moc
 
 # Set tmux preferences in ~/.tmux.conf
-echo tmux.conf >> ~/.tmux.conf
+cat tmux.conf >> ~/.tmux.conf
 
 # neovim preferences to ~/.config
 cp -R nvim/ ~/.config

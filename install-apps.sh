ddb57cb0c5979e585539d52399d450899ed849a9
Joseph Samela <josephsamela@gmail.com>
2019-04-14 16:35:25 -0400

    add app install script

---
+++ install-apps.sh
@@ -0,0 +1,9 @@
+
+# Reminder to run this as root!
+if [ ! "`whoami`" = "root" ]
+then
+    echo "Please run script as root."
+    exit 1
+fi
+
+sudo apt install git moc unzip htop tmux tree neofetch ranger ffmpeg neovim nmap wget curl python3 python3-pip golang

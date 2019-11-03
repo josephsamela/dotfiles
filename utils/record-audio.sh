374209b04645cd71598b0644610861eec7d40ab6
Joseph Samela <josephsamela@gmail.com>
2019-11-03 13:44:51 -0500

    hide ffmpeg output

--- utils/record-audio.sh
+++ utils/record-audio.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-echo "\nRECORDING FROM:  `arecord -l | grep \"card 2\"`"
+
+echo ""
+echo "RECORDING FROM:  `arecord -l | grep "(*)"` "
 FILENAME=`date '+%Y.%m.%d_%H.%M.%S.wav'`
 FILEPATH=`pwd`
-echo "RECORDING TO  :  \"$FILEPATH$FILENAME\""
-echo "Press \"q\" to stop recording\n"
+echo "RECORDING TO  :  $FILEPATH/$FILENAME"
+echo "Press \"q\" to stop recording"
+echo ""
+
+{
 ffmpeg -hide_banner  -f alsa -i hw:1,1 -af acompressor=threshold=-21dB:ratio=9:attack=200:release=1000 $FILENAME
+} &> /dev/null

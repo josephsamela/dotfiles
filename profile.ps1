b89594429a282988fbe2509943fb7fd89e9fa693
Joseph Samela <josephsamela@gmail.com>
2020-04-01 15:42:03 -0400

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,13 @@
+# On a windows computer (with powershell obviously) put this file at:
+# C:\Users\{user}\Documents\WindowsPowerShell\profile.ps1
+
+cd $HOME
+
+Set-Alias -Name vim -Value $HOME\Documents\WindowsPowerShell\nvim-win64\Neovim\bin\nvim.exe
+Set-Alias -Name nvim -Value $HOME\Documents\WindowsPowerShell\nvim-win64\Neovim\bin\nvim.exe
+
+function u { set-location "$HOME" }
+function p { set-location "$HOME\projects" }
+function n { set-location "$HOME\notes" }
+
+function prc { vim $HOME\Documents\WindowsPowerShell\profile.ps1 }
